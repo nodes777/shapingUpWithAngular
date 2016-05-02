ba72dd83f6949842827d24db80fe015aeb603a2f
nodes777 <nodes777@gmail.com>
2016-05-02 14:55:45 -0400

    loading from google books

--- app/app.js
+++ app/app.js
@@ -11,9 +11,23 @@
     };
   });
 
-  app.controller('StoreController', function() {
-    this.products = books;
-  });
+  app.controller('StoreController', [ '$http', function($http) {
+    //this.products = books; //This is the local data, defined below
+    var store = this;
+    store.products = [];
+    var isbns = ["0452286379", "0793821010"];
+    for(var i = 0; i < isbns.length; i++){
+      var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:"+isbns[i]+"&key=AIzaSyBQ9ddCXiRFaB8IoNO3ghXfk4SjlxZizJY";
+        $http.get(url).success( function(data) {
+          console.log(data.items[0].volumeInfo.title);
+          console.log(data.items[0].volumeInfo.authors[0]);
+          console.log(data.items[0].volumeInfo.description);
+          console.log(data.items[0].volumeInfo.pageCount);
+          console.log("ISBN 10: "+isbns);
+          store.products = data;
+       });
+    }
+  }]);
 
 
   app.controller("ReviewController", function(){
@@ -28,11 +42,13 @@
 
   });
 
+
   var books = [{
     name: 'The Fountainhead',
     description: "When The Fountainhead was first published, Ayn Rand's daringly original literary vision and her groundbreaking philosophy, Objectivism, won immediate worldwide interest and acclaim. This instant classic is the story of an intransigent young architect, his violent battle against conventional standards, and his explosive love affair with a beautiful woman who struggles to defeat him.",
     author: "Ayn Rand",
     price: 6.48,
+    isbn: 0452286379,
     rarity: 7,
     color: '#CCC',
     pages: 753,
@@ -58,6 +74,7 @@
     author: "David E. Boruchowitz ",
     price: 4.33,
     rarity: 6,
+    isbn: 0793821010,
     color: '#EEE',
     pages: 254,
     images: [
